3f1254aaee681612e0a47841af03e609faeebf67
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2024-09-26 19:47:10 +0300

    Creating the train-mvc refactoring code

--- components/Commentaire.php
+++ components/Commentaire.php
@@ -4,7 +4,7 @@ require_once "Reaction.php";
 function Commentaire($contenu, $nomCommentataire, $id_publication) {
 ?>
     <div class="flex flex-col gap-4 border border-slate-400 p-4">
-        <div class="flex gap-2 w-full bg-blue-300 p-2 rounded-md" style="width: fit-content;">
+        <div class="flex gap-2 w-full bg-blue-300 p-2 rounded-md text-wrap" style="width: fit-content;">
             <div class="flex gap-4">                                
                 <img 
                     src="../img/face-laugh-solid.svg" 
@@ -19,7 +19,7 @@ function Commentaire($contenu, $nomCommentataire, $id_publication) {
                     <?php echo $nomCommentataire; ?>
                 </span>
                 
-                <p class='contenu text-gray-500 '> 
+                <p class='flex contenu text-gray-500 text-wrap overflow-x-auto bg-red-500'> 
                     <?php echo $contenu; ?> 
                 </p>
             </div>
